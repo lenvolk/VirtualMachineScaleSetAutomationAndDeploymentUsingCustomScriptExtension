262bc9f0497e9498a3c5dc0b48f4162c60e9dc73
Len Volk <levolkov@microsoft.com>
2022-04-29 22:07:10 -0400

    tst

--- LenVolk/vmss_extention.ps1
+++ LenVolk/vmss_extention.ps1
@@ -1,60 +1,62 @@
 
 # Install Notepad++ v7.8.8
 $customConfig = @{
-    "fileUris"         = (, "https://raw.githubusercontent.com/lenvolk/AzVmssExtension/main/notepadv1.ps1");
-    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File notepadv1.ps1"
+    "fileUris"         = (, "https://raw.githubusercontent.com/lenvolk/VirtualMachineScaleSetAutomationAndDeploymentUsingCustomScriptExtension/master/LenVolk/notepad788.ps1");
+    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File notepad788.ps1"
 }
 
 $extensionname = "customScript"
 
 # Get information about the scale set
 $vmss = Get-AzVmss `
-    -ResourceGroupName "JumpBox" `
-    -VMScaleSetName "jumpvm"
+    -ResourceGroupName "myResourceGroup" `
+    -VMScaleSetName "myScaleSet"
 
 # Use Custom Script Extension to install custom extension
-Add-AzVmssExtension -VirtualMachineScaleSet $vmss `
+$vmss = Add-AzVmssExtension 
+-VirtualMachineScaleSet $vmss `
     -Name $extensionname `
     -Publisher "Microsoft.Compute" `
     -Type "CustomScriptExtension" `
-    -TypeHandlerVersion 1.10 `
+    -TypeHandlerVersion 1.9 `
     -Setting $customConfig
 
 # Update the scale set and apply the Custom Script Extension to the VM instances
 Update-AzVmss `
-    -ResourceGroupName "JumpBox" `
-    -Name "jumpvm" `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myScaleSet" `
     -VirtualMachineScaleSet $vmss
 
-#### Update Notepad++
+#### !!! Update Notepad++ to v 8.2.1
 
 $vmss = Get-AzVmss `
-    -ResourceGroupName "JumpBox" `
-    -VMScaleSetName "jumpvm"
+    -ResourceGroupName "myResourceGroup" `
+    -VMScaleSetName "myScaleSet"
 
 # Remove customscrip
 Remove-AzVmssExtension -VirtualMachineScaleSet $vmss -Name $extensionname
 Update-AzVmss `
-    -ResourceGroupName "JumpBox" `
-    -Name "jumpvm" `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myScaleSet" `
     -VirtualMachineScaleSet $vmss
 
  
 
 $customConfigv2 = @{
-    "fileUris"         = (, "https://raw.githubusercontent.com/lenvolk/AzVmssExtension/main/notepadv2.ps1");
-    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File notepadv2.ps1"
+    "fileUris"         = (, "https://raw.githubusercontent.com/lenvolk/VirtualMachineScaleSetAutomationAndDeploymentUsingCustomScriptExtension/master/LenVolk/notepad821.ps1");
+    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File notepad821.ps1"
 }
 
-Add-AzVmssExtension -VirtualMachineScaleSet $vmss `
+$vmss = Add-AzVmssExtension 
+-VirtualMachineScaleSet $vmss `
     -Name $extensionname `
     -Publisher "Microsoft.Compute" `
     -Type "CustomScriptExtension" `
-    -TypeHandlerVersion 1.10 `
+    -TypeHandlerVersion 1.9 `
     -Setting $customConfigv2
 
 # Update the scale set and apply the Custom Script Extension to the VM instances
 Update-AzVmss `
-    -ResourceGroupName "JumpBox" `
-    -Name "jumpvm" `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myScaleSet" `
     -VirtualMachineScaleSet $vmss
