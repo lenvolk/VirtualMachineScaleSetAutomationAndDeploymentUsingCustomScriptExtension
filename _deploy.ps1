8c5ea6fe62ff6103afed5bbd289eb659de72fb83
Len Volk <levolkov@microsoft.com>
2022-05-02 12:50:21 -0400

    working state

--- _deploy.ps1
+++ _deploy.ps1
@@ -4,3 +4,9 @@ Connect-AzAccount -EnvironmentName AzureUSGovernment
 Add-AzAccount
 #Select the correct subscription
 Get-AzSubscription -SubscriptionName "AzIntConsumption" | Select-AzSubscription
+
+
+#### Logs
+# C:\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension\1.9\CustomScriptHandler.txt
+# 
+# C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.12\Downloads\0
